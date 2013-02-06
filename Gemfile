1b6204dcf048c0ecb082abe5aaec2e24641da966
Sam Cooper <sam@chgworks.com>
2013-02-06 14:38:00 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem 'berkshelf'
+gem 'vagrant'
+gem 'jamie'
+gem 'jamie-vagrant'
+gem 'knife-solo', :git => 'git://github.com/matschaffer/knife-solo.git'
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'polipo'
+maintainer       'BlueBox'
+maintainer_email 'scooper@bluebox.met'
+license          'All rights reserved'
+description      'Installs/Configures polipo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
+depends 'yum'
