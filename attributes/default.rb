ec790af8b360b71a4927ba0e415e8fb39a0e2751
Sam Cooper <sam@chgworks.com>
2013-02-08 01:16:43 -0800

    default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 default["polipo"]["allowed_clients"] = "192.168.0.0/16, 10.0.0.0/24"
-default["polipo"]["proxy_ipadress"] = "198.51.100.1"  # fake address
+default["polipo"]["proxy_ipadress"] = "198.168.0.10"
