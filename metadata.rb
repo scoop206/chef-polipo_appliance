4b0e01537371f8c3cad5630d112a73956efd8fb4
Sam Cooper <sam@chgworks.com>
2013-02-09 13:42:37 -0800

    update license in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'polipo'
-maintainer       'BlueBox'
+maintainer       'BlueBox Inc.'
 maintainer_email 'scooper@bluebox.met'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures polipo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
